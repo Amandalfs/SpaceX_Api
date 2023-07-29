b94efdd9653acd04f99ea2ec3c316f317e95248d
Amandalfs <amandalfs57@gmail.com>
2023-07-29 12:42:03 -0300

    chore: adicionar o jest e configuração do jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/src/"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts>"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest"
+  }
+}
+
+export default config
