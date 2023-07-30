cb40b84368ca1f3fed54d278ef5b4300d123ec90
Amandalfs <amandalfs57@gmail.com>
2023-07-30 16:25:44 -0300

    feat: criar interface para getalls

--- src/entities/implements/ilaunchesRepository.ts
+++ src/entities/implements/ilaunchesRepository.ts
@@ -1,10 +1,34 @@
 import { Launch } from "@prisma/client";
 
+interface IGetLaunch {
+    id: string;
+    success: boolean;
+    flight_number: number;
+    date_utc: Date;
+    webcast: string;
+    reused: boolean;
+    name: string;
+    rocket_id: string;
+    rocket: { 
+        id: string, 
+        name: string
+    };
+    Payload: {
+        id: string;
+        name: string;
+        launchId: string;
+    }[];
+}
+
+  
 interface ILaunchesRepository {
     add(data: Launch): Promise<void>
     adds(datas: Launch[]): Promise<void>
+    getAll(page: number, pageSize: number): Promise<IGetLaunch[]>
+    countRows(): Promise<number>
 }
 
 export {
     ILaunchesRepository,
+    IGetLaunch
 }
