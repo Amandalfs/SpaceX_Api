7cfe33975efeded19e766330f2d691600d699788
Amandalfs <amandalfs57@gmail.com>
2023-07-30 18:39:44 -0300

    feat: criar function para pesquisa pelo prisma

--- src/entities/prismaRepositories/prismaLaunchesRepository.ts
+++ src/entities/prismaRepositories/prismaLaunchesRepository.ts
@@ -3,6 +3,7 @@ import { Launch } from '@prisma/client';
 import { prisma } from "../prismaClient";
 
 class PrismaLaunchesRepository implements ILaunchesRepository {
+    
     async add(data: Launch){
         await prisma.launch.create({data});
     }
@@ -29,6 +30,35 @@ class PrismaLaunchesRepository implements ILaunchesRepository {
 
     async countRows(): Promise<number> {
         return prisma.launch.count();
+    }
+
+    async searchLaunch(page: number, pageSize: number, search: string, result: boolean){
+        const launches = await prisma.launch.findMany({
+            where: {
+                name: { contains: search },
+                success: result ,
+                rocket: {
+                    name: { contains: search }
+                }
+            },
+            skip: (page - 1) * pageSize,
+            take: pageSize,
+            include: { rocket: true, Payload: true },
+        })
+        return launches;
+    }
+
+    async countOfSearch(search?: string, result?: boolean): Promise<number> {
+        const launches = await prisma.launch.count({
+            where: {
+                name: { contains: search },
+                success: result ,
+                rocket: {
+                    name: { contains: search }
+                }
+            }
+        })
+        return launches;
     }
 
 }
