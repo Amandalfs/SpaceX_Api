58c11e71e7464f6d1938d8cae8e9f558c1046a2c
Amandalfs <amandalfs57@gmail.com>
2023-07-31 14:23:05 -0300

    chore: configurar o swagger e cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,40 @@
 import express  from "express";
 import { routes } from "./main/routes";
 import { databaseUpdate, task } from "./services/jobs/jobUpdateDatabase";
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API da SpaceX',
+        version: '1.0.0',
+        description: 'Documentação da API da SpaceX',
+      },
+      servers: [
+        {
+          url: `http://localhost:${process.env.PORT}`,
+          description: 'Servidor de Desenvolvimento',
+        },
+        {
+            url:process.env.HOST_PRODUCTION,
+            description: 'Servidor de produção'
+        }
+      ],
+    },
+    apis: ['./src/main/routes/launches.routes.ts', './src/main/routes/launches.routes.ts'],
+};
+  
+const swaggerSpec = swaggerJsdoc(options);
+  
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 databaseUpdate.execute();
 task.start();
