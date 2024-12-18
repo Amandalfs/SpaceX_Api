507a05033901ffbd55e2b15222939545c8812f7d
Amandalfs <amandalfs57@gmail.com>
2024-12-18 10:36:11 -0300

    feat: change time cron at 9:00 am

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { app } from "./app";
-import { databaseUpdate } from "./services/jobs/jobUpdateDatabase";
-import cron from 'node-cron';
+// import { databaseUpdate } from "./services/jobs/jobUpdateDatabase";
+// import cron from 'node-cron';
 
 const PORT = process.env.PORT ?? 8020;
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando na porta ${PORT}`);
-    cron.schedule('0 9 * * *', async () =>{ 
-        databaseUpdate.execute().then(() => {
-            console.log("service run successfully");
-        }).catch(()=>{
-            console.log("service error");
-        });
-    })
+    // cron.schedule('0 9 * * *', async () =>{ 
+    //     databaseUpdate.execute().then(() => {
+    //         console.log("service run successfully");
+    //     }).catch(()=>{
+    //         console.log("service error");
+    //     });
+    // })
 })
